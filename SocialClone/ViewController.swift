a5d13f679e7fddfda6834f11b5d770b0dfe446ce
Mark Chen <lazzyjessie@gmail.com>
2016-11-28 22:27:46 +0800

    add facebook,firebase authenticate

--- SocialClone/ViewController.swift
+++ SocialClone/ViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FirebaseAuth
 class ViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var emailTextField: FancyField!
@@ -19,9 +21,37 @@ class ViewController: UIViewController,UITextFieldDelegate {
         pwdTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func tappedFBLoginBtn(_ sender: UIButton) {
+        let FBLogin = FBSDKLoginManager()
+        
+        FBLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("MARK: Unable to authenticate with facebook - \(error)")
+            }else if result?.isCancelled == true {
+                print("MARK: User cancelled facebook authenticate")
+            }else{
+                print("MARK: Success, authenticate with facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential:FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil{
+                print("MARK: Unable to authenticate with firebase - \(error)")
+            }else {
+                print("MARK: Success, authenticate with firebase")
+            }
+        })
+        
+    }
     //MARK- dismiss textfield
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+        super.touchesBegan(touches, with: event)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
