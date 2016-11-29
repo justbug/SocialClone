b4921bb38e5fd3dd5eed2db431c2442aaa6a453f
Mark Chen <lazzyjessie@gmail.com>
2016-11-30 04:39:03 +0800

    completed email-auth(checkValid)

--- SocialClone/ViewController.swift
+++ SocialClone/ViewController.swift
@@ -11,7 +11,7 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 import FirebaseAuth
 class ViewController: UIViewController,UITextFieldDelegate {
-
+    
     @IBOutlet weak var emailTextField: FancyField!
     @IBOutlet weak var pwdTextField: FancyField!
     
@@ -22,7 +22,13 @@ class ViewController: UIViewController,UITextFieldDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func tappedFBLoginBtn(_ sender: UIButton) {
+    @IBAction func signInBtnTapped(_ sender:UIButton) {
+        self.checkTextfieldTypingError {
+            self.firebaseAuthEmail()
+        }
+    }
+    
+    @IBAction func FBLoginBtnTapped(_ sender: UIButton) {
         let FBLogin = FBSDKLoginManager()
         
         FBLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
@@ -33,12 +39,36 @@ class ViewController: UIViewController,UITextFieldDelegate {
             }else{
                 print("MARK: Success, authenticate with facebook")
                 let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-                self.firebaseAuth(credential)
+                self.firebaseAuthCredential(credential)
             }
         }
     }
     
-    func firebaseAuth(_ credential:FIRAuthCredential){
+    func checkTextfieldTypingError(completed: ()->()){
+        
+        let alertViewController = UIAlertController(title: "登入失敗", message: "", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "重新輸入", style: .default, handler: nil)
+        alertViewController.addAction(alertAction)
+        
+        if emailTextField.text == "" || pwdTextField.text == "" {
+            alertViewController.message = "電子郵件或密碼有一欄為空值"
+            self.present(alertViewController, animated: true, completion: nil)
+        }else {
+            if !(emailTextField.text?.isValidEmail())! {
+                alertViewController.message = "電子郵件格式不正確"
+                self.present(alertViewController, animated: true, completion: nil)
+            }else  if (pwdTextField.text?.characters.count)! < 6{
+                alertViewController.message = "密碼不能少於5個字元"
+                self.present(alertViewController, animated: true, completion: nil)
+            }else {
+                completed()
+            }
+        }
+    }
+    
+    // MARK: - firebaseAuth
+    
+    func firebaseAuthCredential(_ credential:FIRAuthCredential){
         FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
             if error != nil{
                 print("MARK: Unable to authenticate with firebase - \(error)")
@@ -48,7 +78,28 @@ class ViewController: UIViewController,UITextFieldDelegate {
         })
         
     }
-    //MARK- dismiss textfield
+    
+    func firebaseAuthEmail() {
+        if let email = emailTextField.text,let pwd = pwdTextField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("MARK: (Email) User authenticate with firebase")
+                }else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("MARK: (Email) User can't authenticate with firebase")
+                            print(error)
+                        }else {
+                            print("MARK: (Email) User successfully create a new account")
+                        }
+                    })
+                }
+            })
+        }
+    }
+
+    // MARK: - dismiss textfield
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
         super.touchesBegan(touches, with: event)
@@ -59,6 +110,12 @@ class ViewController: UIViewController,UITextFieldDelegate {
         pwdTextField.resignFirstResponder()
         return true
     }
-
+    
 }
 
+extension String {
+    func isValidEmail() -> Bool{
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
+    }
+}
